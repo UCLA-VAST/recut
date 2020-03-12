c3a32d194736ee32f6f6b5a713bd187eca099ed6
Karl Marrett <kdmarrett@gmail.com>
2020-03-12 14:45:29 -0700

    common

---
+++ include/mcp3d_common.hpp
@@ -0,0 +1,22 @@
+//
+// Created by muyezhu on 9/19/17.
+//
+
+#ifndef MCP3D_CORE_HPP
+#define MCP3D_CORE_HPP
+
+// integer and float type declarations
+// and numeric limits
+#include <cstdint>
+#include <cfloat>
+// any header included here must have
+// no dependency on other files in
+// mcp3d codebase
+#include "mcp3d_macros.hpp"
+#include "mcp3d_types.hpp"
+#include "mcp3d_constants.hpp"
+#include "mcp3d_paths.hpp"
+#include "mcp3d_utility.hpp"
+#include "mcp3d_geometry.hpp"
+
+#endif //MCP3D_CORE_HPP
