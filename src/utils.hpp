49e36bc719772111f465d2ee29b8f5f783f72225
Mrunal Patel <mkpatel@cs.ucla.edu>
2020-03-20 14:54:56 -0700

    updated utils.hpp to use stdlib and added TODOs (which are mostly done) to README

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,9 +1,9 @@
 
-#include<boost/filesystem.hpp>
-using namespace boost::filesystem;
+#include <filesystem>
 
-std::string get_curr() {
-  path full_path(current_path());
-  return canonical(full_path).string();
+namespace fs = std::filesystem;
+
+string get_curr() {
+    path fs::current_path();
+    return fs::canonical(path).string();
 }
-
