b5c2c37ffef6a3426cd5e19d984e7deba2550853
Karl Marrett <kdmarrett@gmail.com>
2020-03-20 15:00:53 -0700

    fullpath

--- src/utils.hpp
+++ src/utils.hpp
@@ -5,7 +5,6 @@ using std::filesystem::remove_all;
 using std::filesystem::directory_iterator;
 using std::filesystem::canonical;
 using std::filesystem::current_path;
-using std::filesystem::full_path;
 
 namespace fs = std::filesystem;
 
