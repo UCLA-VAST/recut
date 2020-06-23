5f31482e552929acb6d22dfdb97843d35ebacd68
Karl Marrett <kdmarrett@gmail.com>
2020-06-23 08:01:04 -0700

    updates to shell env

--- shell.nix
+++ shell.nix
@@ -1,4 +1,6 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
-  buildInputs = [ pkgs.openssl pkgs.boost pkgs.libtiff ];
+  buildInputs = [ pkgs.openssl pkgs.boost pkgs.libtiff
+    pkgs.clang-tools pkgs.python38Packages.pynvim pkgs.nodejs pkgs.cmake ];
 }
+
