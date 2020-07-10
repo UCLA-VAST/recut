1625cb0da5bfbf8220478a4a50c8a8d0aff4a377
Karl Marrett <kdmarrett@gmail.com>
2020-07-09 17:24:19 -0700

    move test directories from bin to data

--- src/interval.hpp
+++ src/interval.hpp
@@ -178,7 +178,7 @@ class Interval {
       in_mem_ = false;
       // open output
       // output is now specific to the interval
-      fn_ = get_curr() + "/interval" + std::to_string(interval_id_) + ".bin";
+      fn_ = get_data_dir() + "/interval" + std::to_string(interval_id_) + ".bin";
 #ifdef FULL_PRINT
       cout << "SaveToDisk() fn: " << fn_ << '\n';
 #endif
