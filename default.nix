b3338c7bc1e340b4c9b80d5d78ccfd2b40cbf0dd
Karl Marrett <kdmarrett@gmail.com>
2020-11-21 09:25:56 -0800

    clean default.nix

--- default.nix
+++ default.nix
@@ -2,18 +2,19 @@ let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
 in
-pkgs.stdenv.mkDerivation {
+  with pkgs;
+stdenv.mkDerivation {
   name = "recut";
 
   # https://nixos.org/nix/manual/#builtin-filterSource
   src = builtins.filterSource 
-    (path: type: pkgs.lib.cleanSourceFilter path type 
+    (path: type: lib.cleanSourceFilter path type 
     && baseNameOf path != "build"
     && baseNameOf path != "bin/*"
     && baseNameOf path != "data/*") ./.;
 
   cmakeFlags = ["-DFROM_NIX_BUILD=ON"];
-  nativeBuildInputs = [ pkgs.cmake ];
+  nativeBuildInputs = [ cmake ];
 
   # used for automated github testing 
   # see .github/workflows/*.yaml
@@ -21,30 +22,19 @@ pkgs.stdenv.mkDerivation {
   enableParallelBuilding = true;
 
   buildInputs = [ 
-    pkgs.python38Packages.matplotlib 
-    pkgs.gtest
-    pkgs.gbenchmark
-    pkgs.range-v3
+    python38Packages.matplotlib 
+    gtest
+    gbenchmark
+    range-v3
     # warning leaving breakpointHook on 
     # can create many sleeping processes on your system
     # it will cause github actions to hang, if there are any failures
-    #pkgs.breakpointHook
-    #openssl 
-    #boost 
-    #libtiff 
-    #unstable.mpich 
-    #clang_10 
-    #llvmPackages.openmp 
+    #breakpointHook
   ];
-
-  buildPhase = "make -j 8 && make install -j 8";
-
-  # an environment variable which can be used within CMakelists or source
-  #IN_NIX_BUILD_ENV="ON";
 
   # You have to run the install step first such that the relevant files used at runtime
   # are in the data directory before running any other tests
-  checkPhase="pwd; ls ../; echo; ls ../data; ./recut_test --gtest_filter=Install.\"*\"; ./recut_test --gtest_output=json:../data/test_detail.json | tee ../data/test_detail.log";
+  checkPhase="./recut_test --gtest_filter=Install.\"*\"; ./recut_test --gtest_output=json:../data/test_detail.json | tee ../data/test_detail.log";
 
   installPhase = ''
     mkdir -p $out/bin
