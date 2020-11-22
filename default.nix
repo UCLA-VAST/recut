684bc8dda7eb644deace0da1e82a2b8a14d20b5e
Karl Marrett <kdmarrett@gmail.com>
2020-11-21 17:09:00 -0800

    fix: failing nix-build

--- default.nix
+++ default.nix
@@ -29,7 +29,8 @@ stdenv.mkDerivation {
     # warning leaving breakpointHook on 
     # can create many sleeping processes on your system
     # it will cause github actions to hang, if there are any failures
-    #breakpointHook
+    # always comment it out before pushing
+    breakpointHook
   ];
 
   # You have to run the install step first such that the relevant files used at runtime
@@ -37,7 +38,9 @@ stdenv.mkDerivation {
   checkPhase="./recut_test --gtest_filter=Install.\"*\"; ./recut_test --gtest_output=json:../data/test_detail.json | tee ../data/test_detail.log";
 
   installPhase = ''
+    pwd
+    ls
     mkdir -p $out/bin
-    cp /build/recut-pipeline/bin/recut_test $out/bin/recut_test
+    cp /build/recut-pipeline/build/recut_test $out/bin/recut_test
   '';
 }
