4139e52b919593d4b6b5df3fbeeb0f89ae1dd367
Pasha Khosravi <khosravipasha@users.noreply.github.com>
2020-12-02 13:36:58 -0800

    comment out breakpoint

--- default.nix
+++ default.nix
@@ -30,7 +30,7 @@ stdenv.mkDerivation {
     # can create many sleeping processes on your system
     # it will cause github actions to hang, if there are any failures
     # always comment it out before pushing
-    breakpointHook
+    # breakpointHook
   ];
 
   # You have to run the install step first such that the relevant files used at runtime
