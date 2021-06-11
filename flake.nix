8890a039b79e367cf610eb82940d89a274163396
Karl Marrett <kdmarrett@gmail.com>
2021-06-11 06:38:11 -0700

    fix: point to mcp3d master

--- flake.nix
+++ flake.nix
@@ -2,14 +2,13 @@
   description = "recut";
   inputs = {
     pinned.url = "github:NixOS/nixpkgs/733e537a8ad76fd355b6f501127f7d0eb8861775";
-    # mcp3d fails on latest unstable hdf5 API changes
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    mcp3d.url = "git+ssh://git@github.com/muyezhu/mcp3d?ref=stable_lib";
-    #openvdb.url = "git+ssh://git@github.com/UCLA-VAST/openvdb?ref=pointalias";
+    # mcp3d.url = "git+ssh://git@github.com/muyezhu/mcp3d?ref=master";
+    mcp3d.url = "git+ssh://git@github.com/muyezhu/mcp3d";
     openvdb.url = "git+ssh://git@github.com/UCLA-VAST/openvdb";
     # alternatively you could pin a certain commit like:
     # mcp3d.url = "git+ssh://git@github.com/muyezhu/mcp3d?ref=kdm-dev&rev=<commit hash>";
-    # or you could use your local filesystem branch with:
+    # or you could with your local filesystem branch with:
     # mcp3d.url = "/home/kdmarrett/mcp3d";
 
     # pin nix package manager versions to exact match between mcp3d and recut
