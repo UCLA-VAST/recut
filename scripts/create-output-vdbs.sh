aaae89007f9c9379f5cd3fa8911df248b5abbcb7
Karl Recon2 <kdmarrett@gmail.com>
2022-03-23 21:24:35 -0700

    x

--- scripts/create-output-vdbs.sh
+++ scripts/create-output-vdbs.sh
@@ -1,4 +1,6 @@
 for ims_file in $1/*.ims ; do
   # run both channels at once
-  recut $ims_file --convert --input-type ims --output-type uint8 --fg-percent .05 & recut $ims_file --convert --input-type ims --output-type mask --fg-percent 10 --channel 1
+  recut $ims_file --convert --input-type ims --output-type uint8 --fg-percent 1 & recut $ims_file --convert --input-type ims --output-type mask --fg-percent 10 --channel 1
+  echo; echo "Finished $ims_file ... deleting"
+  rm $ims_file
 done
