7aa88abd15540a83659a2d4f851aaba2fd10a105
Karl Recon2 <kdmarrett@gmail.com>
2022-04-06 14:26:12 -0700

    feat: filter swcs start

--- scripts/batch-run-recut-threads.sh
+++ scripts/batch-run-recut-threads.sh
@@ -10,24 +10,37 @@ do
   echo $DIR
   cd $DIR
 
-  if [ ! -f point.vdb ]
-  then
-  #recut ../$d$EXT_PATH --convert point.vdb --input-type tiff --output-type point --parallel 24
-  for thread_count in 1 2 4 8 16 24 32 48
+  #if [ ! -f point.vdb ]
+  #then
+  #recut ../$d$EXT_PATH --convert point.vdb --input-type tiff --output-type point --parallel 1
+  for thread_count in 1
   do
-    recut ../$d$EXT_PATH --convert point.vdb --input-type tiff --output-type point --parallel $thread_count
-    echo "finished thread_count: $thread_count"
-  done
+  #if [ ! -f point.vdb-log-$thread_count.txt ]
+  if [ $thread_count -eq 100 ]
+  then
+    recut ../$d$EXT_PATH --seeds marker_files --parallel $thread_count --output-windows uint8.vdb --run-app2
+  else
+    recut ../$d$EXT_PATH --seeds marker_files --parallel $thread_count 
+	  WAIT_TIME=60
+	  MINUTES=0
+	  MAX_MINUTES=30
+	  until [ $MINUTES -eq $MAX_MINUTES ] || recut ../$d$EXT_PATH --seeds marker_files --parallel $thread_count; do
+		      sleep $WAIT_TIME
+		      $(( MINUTES++ ))
+          done
   fi
+  echo "finished thread_count: $thread_count"
+  done
+  #fi
 
-  recut point.vdb --seeds marker_files --output-windows uint8.vdb --run-app2 --parallel 1
   #if [ ! -d run-1 ]
   #then
+  #recut point.vdb --seeds marker_files --output-windows uint8.vdb --parallel 1 --run-app2 
+  #fi
   #for thread_count in 1 2 4 8 16 24 32 48
   #do
   #  recut point.vdb --seeds marker_files --input-type point --parallel $thread_count
   #done
-  #fi
 
   cd ..
 done
---
+++ scripts/filter-swcs.py
@@ -0,0 +1,5 @@
+import os
+import sys
+
+print(sys.argv[1])
+for root, dirs,os,walk(sys.argv[1])
