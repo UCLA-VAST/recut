bc59b174792a0addfcbe0e28c78d10282fdf5174
Karl Marrett <kdmarrett@gmail.com>
2022-08-24 15:11:48 -0700

    feat: start imaris annotations conversion

---
+++ scripts/convert_imaris_annotations.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from pandas import read_csv
+import sys
+
+# annotation file that contains soma locations only
+annotations = Path(sys.argv[1])
+recut = annotations.parent / 'soma_recut'
+recut.mkdir(exist_ok=True)
+annotations_df = read_csv(annotations)
+SOMA_SIZE = 12
+
+for column in ("x", "y", "z", "volsize"):
+    annotations_df[column] = annotations_df[column].round(decimals=0).astype(int)
+
+for row in annotations_df.itertuples():
+    with open(recut/f"marker_{row.x}_{row.y}_{row.z}_{row.volsize}", 'w') as soma_file:
+        soma_file.write("# x,y,z,radius\n")
+        soma_file.write(f"{row.x},{row.y},{row.z}")
