57f473320b314c62311c55e03e42a910bda3ab7f
Keivan Moradi <k.moradi@gmail.com>
2022-10-11 20:25:57 -0700

    * add radius to marker files

--- scripts/convert_terafly_annotations.py
+++ scripts/convert_terafly_annotations.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from pandas import read_csv
 import sys
+from math import pi
 
 # annotation file that contains soma locations only
 annotations = Path(sys.argv[1])
@@ -13,5 +14,5 @@ for column in ("x", "y", "z", "volsize"):
 
 for row in annotations_df.itertuples():
     with open(recut/f"marker_{row.x}_{row.y}_{row.z}_{row.volsize}", 'w') as soma_file:
-        soma_file.write("# x,y,z\n")
-        soma_file.write(f"{row.x},{row.y},{row.z}")
+        soma_file.write("# x,y,z,radius_um\n")
+        soma_file.write(f"{row.x},{row.y},{row.z},{(row.volsize*3/4/pi)**(1/3)}")
