347272be0ef08e9c6cad0da508c60e8c5d53d911
Karl Marrett <kdmarrett@gmail.com>
2022-10-24 10:36:52 -0700

    feat: connect precision and recall to recut_qa

--- scripts/recut_qa.py
+++ scripts/recut_qa.py
@@ -1,6 +1,7 @@
 import subprocess
 import argparse
 import re
+from test_precision_recall import precision_recall
 
 def parse_range(string):
     if string.contains('-'):
@@ -16,23 +17,40 @@ def parse_range(string):
 
 
 def call_recut(**kwargs):
-    exclude = ['image', 'known-seeds'] 
-    args = "".join([f"--{k} {v} ".replace('_', '-') for k,v in kwargs.items() if k not in exclude if v])
-    cmd = f"recut {kwargs['image']} {args}"
-    print(cmd)
-    # subprocess.run(cmd, shell=True)
+    if kwargs['inferenced_path'] is None:
+        # whitelist certain arguments to pass directly to recut
+        include = ['min_radius', 'max_radius', 'open_denoise', 'open_steps', 'close_steps', 'fg_percent']
+        args = "".join([f"--{k} {v} ".replace('_', '-') for k,v in kwargs.items() if k in include if v])
+        cmd = f"recut {kwargs['image']} {args} --voxel-size {kwargs['voxel_size_x']} {kwargs['voxel_size_y']} {kwargs['voxel_size_z']}"
+        print(cmd)
+
+        output = subprocess.check_output(cmd.split()).strip().decode().split('\n')
+        run_dir = [v.split()[-1] for v in output if "written to:" in v][0]
+        kwargs['inferenced_path'] = f"{run_dir}/seeds"
+
+    precision_recall(**kwargs)
+
     # return an object that will be a row of a pandas df for soma_recall, soma_precision, neuron yield, neuron precision, run, git commit, voxel size, for this given run
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('image')
-    parser.add_argument('known-seeds', help='Evaluate recut somas against ground truth')
-    parser.add_argument('--min-radius', type=parse_range)
-    parser.add_argument('--max-radius', type=parse_range)
-    parser.add_argument('--open-denoise', type=parse_range)
-    parser.add_argument('--open-steps', type=parse_range)
-    parser.add_argument('--close-steps', type=parse_range)
-    parser.add_argument('--fg-percent', type=parse_range)
+    # explicitly for precision and recall
+    parser.add_argument('labeled_path', help='Path to ground truth somas with which to compare the inferences to')
+    parser.add_argument('--inferenced-path', help="Use an existing input path run for inferenced soma location")
+    parser.add_argument("--distance-threshold", type=float, default=17,
+                        help="a minimum allowed distance between inferences and ground truth in µm "
+                             "to consider them a match.")
+    # recut arguments
+    parser.add_argument('--min-radius', type=float, help="a min allowed radii for inferences for filtering purpose")
+    parser.add_argument('--max-radius', type=float, help="a max allowed radii for inferences for filtering purpose")
+    parser.add_argument('--open-denoise', type=float)
+    parser.add_argument('--open-steps', type=float)
+    parser.add_argument('--close-steps', type=float)
+    parser.add_argument('--fg-percent', type=float)
+    parser.add_argument("--voxel-size-x", type=float, default=1, help="x voxel size in µm.")
+    parser.add_argument("--voxel-size-y", type=float, default=1, help="y voxel size in µm.")
+    parser.add_argument("--voxel-size-z", type=float, default=1, help="z voxel size in µm.")
     args = parser.parse_args()
 
     call_recut(**vars(args))
--- scripts/test_precision_recall.py
+++ scripts/test_precision_recall.py
@@ -84,7 +84,7 @@ def euc_distance(
     return euc_dist
 
 
-def is_match(euc_dist: List[Tuple[int, ...]]):
+def is_match(euc_dist: List[Tuple[int, ...]], distance_threshold):
     """
     a function to decide match/non-match
 
@@ -101,19 +101,39 @@ def is_match(euc_dist: List[Tuple[int, ...]]):
     match_list = []
     for soma_inf in euc_dist:
         soma_inf_lst = list(soma_inf)
-        is_match_each = [1 if d <= DISTANCE_THRESH else 0 for d in soma_inf_lst]
+        is_match_each = [1 if d <= distance_threshold else 0 for d in soma_inf_lst]
         match_list.append(tuple(is_match_each))
     return match_list
 
 
-def precision_recall():
+def precision_recall(**kwargs):
     """
     calculate the precision and recall, and print them out
 
     """
+
+    # radii of the label (um)
+    distance_threshold = kwargs['distance_threshold']
+    print(f"distance threshold: {distance_threshold} µm\n"
+          f"voxel_sizes: x={kwargs['voxel_size_x']}, y={kwargs['voxel_size_y']}, z={kwargs['voxel_size_z']} µm")
+
+    labeled_path = kwargs['labeled_path']
+    inferenced_path = Path(kwargs['inferenced_path'])
+
+    # directory that stores the precision & recall summary file and the plots
+    time_now = datetime.now()
+    current_time = time_now.strftime("%H:%M:%S").replace(':', '_')
+    path_result = inferenced_path.parent / f"result_run_{current_time}"
+    path_result.mkdir(exist_ok=True)
+
+    # gather markers
+    label, label_with_radii = gather_markers(labeled_path)
+    inference, inference_with_radii = gather_markers(
+        inferenced_path, max_radius=kwargs['max_radius'], min_radius=kwargs['min_radius'])
+
     euc_dists = euc_distance(label, inference,
-                            voxel_sizes=array([args.voxel_size_x, args.voxel_size_y, args.voxel_size_z], dtype=float))
-    matches = is_match(euc_dists)  # all matches and unmatches
+                            voxel_sizes=array([kwargs['voxel_size_x'], kwargs['voxel_size_y'], kwargs['voxel_size_z']], dtype=float))
+    matches = is_match(euc_dists, distance_threshold)  # all matches and unmatches
 
     # if two identical macthes, only remain the one with the lowest Euclidean distance as the match
     # and set others to 0 (non-match)
@@ -226,9 +246,8 @@ def precision_recall():
           f"Total number of true labels: {n_pairs}, Total number of inferences: {len(match)}")
 
     # save matched pairs and all information of this run to a txt file
-    # print(current_time)
     with open(path_result / f"precision_recall_{current_time}.txt", 'w') as f:
-        f.write(f"Distance threshold: {DISTANCE_THRESH}\n")
+        f.write(f"Distance threshold: {distance_threshold}\n")
         # f.write(f"Number of duplicate match of true labels: {num_dup_match}\n")
         f.write(f"TP: {TP}, FP: {FP}, FN: {FN}\n")
         f.write(f"Precision: {precision*100:.2f}%, Recall: {recall*100:.2f}%\n")
@@ -254,9 +273,9 @@ def precision_recall():
         FN_x = FN[0]
         FN_y = FN[1]
         FN_z = FN[2]
-        with open(path_FN_markers / f"marker_{FN_x}_{FN_y}_{FN_z}_{int(round(RADII, 0))}", 'w') as marker_file:
+        with open(path_FN_markers / f"marker_{FN_x}_{FN_y}_{FN_z}_{int(round(distance_threshold, 0))}", 'w') as marker_file:
             marker_file.write("# x,y,z,radius\n")
-            marker_file.write(f"{FN_x},{FN_y},{FN_z},{RADII}")
+            marker_file.write(f"{FN_x},{FN_y},{FN_z},{distance_threshold}")
 
     # generate and ano file for visualization in TeraFly
     FP_df = pd.DataFrame(FP_list, columns=("x", "y", "z"))
@@ -265,7 +284,7 @@ def precision_recall():
 
     FN_df = pd.DataFrame(FN_list, columns=("x", "y", "z"))
     FN_df["comment"] = "FN"
-    FN_df["volsize"] = RADII**3 * 4/3 * np.pi
+    FN_df["volsize"] = distance_threshold**3 * 4/3 * np.pi
 
     TP_df = pd.DataFrame(TP_list, columns=("x", "y", "z"))
     TP_df["comment"] = "TP"
@@ -294,10 +313,10 @@ def precision_recall():
 
     df.to_csv(path_result/"test.csv", index_label="n")
     # generate histogram and cdf for TP and FP
-    plot_his_cdf(inference_coord_radii_list, FP_coord_radii_list)
+    plot_his_cdf(inference_coord_radii_list, FP_coord_radii_list, distance_threshold, current_time, path_result)
 
 
-def plot_his_cdf(inference_coord_radii_list, FP_coord_radii_list):
+def plot_his_cdf(inference_coord_radii_list, FP_coord_radii_list, distance_threshold, current_time, path_result):
     """
     a function to plot the histogram and CDF for TPs and FPs
     :param inference_coord_radii_list:
@@ -408,7 +427,7 @@ def plot_his_cdf(inference_coord_radii_list, FP_coord_radii_list):
 
     ### 5,6 plot scatter plots of TP/FP radii vs. true label radii
     axs[2,0].scatter(np.linspace(0,1,len(TP_radii)), TP_radii, label='TPs', color='blue')
-    axs[2,0].axhline(y=RADII, linestyle='--', c='green')
+    axs[2,0].axhline(y=distance_threshold, linestyle='--', c='green')
     axs[2,0].set_ylim(bottom=0, top=radii_max)
     axs[2,0].legend(loc='upper right')
     axs[2,0].set_xlabel('Sample')
@@ -416,7 +435,7 @@ def plot_his_cdf(inference_coord_radii_list, FP_coord_radii_list):
     axs[2,0].set_title("Distribution of radii of TPs compared to labels' radii")
     
     axs[2,1].scatter(np.linspace(0,1,len(FP_radii)), FP_radii, label='FPs', color='red')
-    axs[2,1].axhline(y=RADII, linestyle='--', c='green')
+    axs[2,1].axhline(y=distance_threshold, linestyle='--', c='green')
     axs[2,1].set_ylim(bottom=0, top=radii_max)
     axs[2,1].legend(loc='upper right')
     axs[2,1].set_xlabel('Sample')
@@ -464,50 +483,23 @@ def plot_his_cdf(inference_coord_radii_list, FP_coord_radii_list):
     plt.savefig(path_result/f"histogram_cdf_{current_time}.pdf", format="pdf")
     plt.show()  
 
-
-if __name__ == "__main__":
-    time_now = datetime.now()
-    current_time = time_now.strftime("%H:%M:%S").replace(':', '_')
+def main():
     # args
     parser = argparse.ArgumentParser()
-    parser.add_argument('labeled_path', help="input path for labeled soma location")
-    parser.add_argument('inferenced_path', help="input path for inferenced soma location")
-    parser.add_argument("--distance_threshold", type=float, default=15,
+    parser.add_argument('labeled-path', help="input path for labeled soma location")
+    parser.add_argument('inferenced-path', help="input path for inferenced soma location")
+    parser.add_argument("--distance-threshold", type=float, default=17,
                         help="a minimum allowed distance between inferences and ground truth in µm "
                              "to consider them a match.")
-    parser.add_argument("--radii_max_threshold", type=float, default=None,
-                        help="a maximum allowed radii for inferences for filtering purpose.")
-    parser.add_argument("--radii_min_threshold", type=float, default=None,
-                        help="a minimum allowed radii for inferences for filtering purpose.")
-    parser.add_argument("--voxel_size_x", type=float, default=1, help="x voxel size in µm.")
-    parser.add_argument("--voxel_size_y", type=float, default=1, help="y voxel size in µm.")
-    parser.add_argument("--voxel_size_z", type=float, default=1, help="z voxel size in µm.")
+    parser.add_argument('--min-radius', type=float, help="a min allowed radii for inferences for filtering purpose")
+    parser.add_argument('--max-radius', type=float, help="a max allowed radii for inferences for filtering purpose")
+    parser.add_argument("--voxel-size-x", type=float, default=1, help="x voxel size in µm.")
+    parser.add_argument("--voxel-size-y", type=float, default=1, help="y voxel size in µm.")
+    parser.add_argument("--voxel-size-z", type=float, default=1, help="z voxel size in µm.")
     args = parser.parse_args()
 
-    DISTANCE_THRESH = args.distance_threshold
-    print(f"distance threshold: {DISTANCE_THRESH} µm\n"
-          f"voxel_sizes: x={args.voxel_size_x}, y={args.voxel_size_y}, z={args.voxel_size_z} µm")
-    # radii of the label (um)
-    RADII = DISTANCE_THRESH
-
-    labeled_path = args.labeled_path
-    inferenced_path = Path(args.inferenced_path)
-
-    # directory that stores the precision & recall summary file and the plots
-    path_result = inferenced_path.parent / f"result_run_{current_time}"
-    path_result.mkdir(exist_ok=True)
-
-    # gather markers
-    label, label_with_radii = gather_markers(labeled_path)
-    inference, inference_with_radii = gather_markers(
-        inferenced_path, max_radius=args.radii_max_threshold, min_radius=args.radii_min_threshold)
-
     # calculate precision & recall
-    precision_recall()
+    precision_recall(**vars(args))
 
-# # for testing purpose
-# label, label_with_radii = gather_markers('C:\\Users\\yanyanming77\\Desktop\\precision_recall\\marker_truth_full')
-# inference, inference_with_radii = gather_markers('C:\\Users\\yanyanming77\\Desktop\\precision_recall\\marker_ms_full')
-# path = Path("C:\\Users\\yanyanming77\\Desktop\\precision_recall")
-# path_result = path/f'result_run_{current_time}'
-# path_result.mkdir(exist_ok = True)
+if __name__ == "__main__":
+    main()
