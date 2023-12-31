1343609cd4f316a5f3a6c114eeaa2d1f0e5a8cff
Karl Marrett <kdmarrett@gmail.com>
2023-12-31 03:55:53 -0800

    scripts: 311 batch run script

--- scripts/multi-recut-run.sh
+++ scripts/multi-recut-run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+timeout 6h recut images/04_Ex_647_Em_690_deconvolved --fg-percent .1 --seeds proofread-swcs --coarsen-steps 2
+timeout 6h recut images/04_Ex_647_Em_690_deconvolved --fg-percent .1 --seeds proofread-swcs --coarsen-steps 0
+#recut images --fg-percent .1 --output-type seeds
+
+#recut vdbs/img-mask-fgpct-0.100.vdb --fg-percent .1 --seeds Inspected_FNT_renamed_sorted
+#recut vdbs/img-mask-fgpct-0.100.vdb --fg-percent .1 --output-type seeds
+#recut vdbs/img-mask-fgpct-0.100.vdb --fg-percent .1 --seeds Inspected_FNT_renamed_sorted -pl
 #timeout 6h recut vdbs/img-mask-fgpct-0.100.vdb --fg-percent .1 --seeds Inspected_FNT_renamed_sorted
 #timeout 6h recut vdbs/img-mask-fgpct-0.200.vdb --fg-percent .2 --seeds Inspected_FNT_renamed_sorted
 #timeout 6h recut vdbs/img-mask-fgpct-0.400.vdb --fg-percent .4 --seeds Inspected_FNT_renamed_sorted
@@ -7,4 +14,4 @@
 #timeout 6h recut vdbs/img-mask-fgpct-1.600.vdb --fg-percent 1.6 --seeds Inspected_FNT_renamed_sorted
 
 # app2
-timeout 3d recut vdbs/img-mask-fgpct-0.200.vdb --fg-percent .2 --seeds Inspected_FNT_renamed_sorted --run-app2 --output-windows vdbs/img-uint8-fgpct-0.200.vdb
+#timeout 3d recut vdbs/img-mask-fgpct-0.200.vdb --fg-percent .2 --seeds Inspected_FNT_renamed_sorted --run-app2 --output-windows vdbs/img-uint8-fgpct-0.200.vdb
