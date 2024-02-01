9b8085870a68c31511e3d4098d827b34a19d4142
Karl Marrett <kdmarrett@gmail.com>
2024-02-01 10:26:24 -0800

    scripts: tests the effect of removing perceived z-smear on reconstruction accuracy by creating downsampled z versions of the image, note this simply drops every N z-planes to achieve downsampling and is only for testing

---
+++ scripts/downsample-z.py
@@ -0,0 +1,23 @@
+import argparse
+from glob import glob
+from shutil import copy
+from os import makedirs
+
+def downsample(args):
+    out = args.input + "_downsample" + str(args.factor)
+    print(out)
+    pattern = args.input + "/*.tif"
+    tifs = sorted(glob(pattern, recursive=False))[0::args.factor]
+    makedirs(out, exist_ok=True)
+    for i, src in enumerate(tifs):
+        dst = out + "/img_" + str(i).zfill(6) + ".tif"
+        print(src, ' -> ', dst)
+        copy(src, dst) 
+    print("Remember to multiply the z voxel size by " + str(args.factor) + " when inputting to reconstruction")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help='Folder of tif files')
+    parser.add_argument('-f', '--factor', help='downsample factor', default=2, type=int)
+    args = parser.parse_args()
+    downsample(args)
